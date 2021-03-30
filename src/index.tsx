80333cd06cef692ef2ea70c0f25bc422715cbe83
Sergey Danilov <largobox@yandex.ru>
2021-03-30 11:00:34 +0300

    check git auth ssh

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 ReactDOM.render(
-  <h1>Maybe some typescript?</h1>,
+  <h1>Maow, maybe some typescript?</h1>,
   document.getElementById('root')
 );
